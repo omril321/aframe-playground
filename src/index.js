ab8589eb9d2863cd3a06eb03c8ccf2eff316f3e1
Omri Lavi <omril321@gmail.com>
2019-07-06 17:30:59 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const hi = 'h123i';
+console.log(`${hi}`);
+
+import html from './index.html';
+
+console.log('HTML: ', html);
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const resolve = require('path').resolve;
+const src = resolve(__dirname, 'src');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+
+    mode: 'none',
+    output: {
+        path: resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(html)$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        attrs: [':data-src']
+                    }
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: [src],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+        ]
+    },
+
+    devServer: {
+        historyApiFallback: true,
+        host: 'localhost',
+        port: 3456,
+        contentBase: "dist",
+        hot: true,
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000,
+            ignored: /node_modules/
+        }
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
+
+};
