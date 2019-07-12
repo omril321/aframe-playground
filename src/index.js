fadfc767dcb6fd4a583d79bb0bcb1ea5e1b2be3b
Omri Lavi <omril321@gmail.com>
2019-07-12 11:43:44 +0300

    integrated with aframe physics

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import "aframe";
+import "aframe-physics-system";
 
-AFRAME.registerComponent('initial-speed', {
+AFRAME.registerComponent('initial-force', {
     schema: {type: "vec3"},
-    tick: function () {
-        const currentPosition = this.el.getAttribute('position');
-        currentPosition.x += this.data.x;
-        currentPosition.y += this.data.y;
-        currentPosition.z += this.data.z;
+    init: function() {
+        const {x, y, z} = this.data;
+        this.el.addEventListener('body-loaded', (e) => {
+            debugger;
+            e.detail.body.applyForce(new CANNON.Vec3(x, y, z),new CANNON.Vec3(0, 0, 0));
+        });
 
-        this.el.setAttribute('position', currentPosition);
-    }
+
+    },
 })
