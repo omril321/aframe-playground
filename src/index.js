04e9aee88c27cc0c6390a2f9c7c975b24aca7d71
Omri Lavi <omril321@gmail.com>
2019-07-18 18:11:51 +0300

    added component 'pushed by looking'

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import "aframe";
-import "aframe-physics-system";
+import 'aframe';
+import 'aframe-physics-system';
 
 AFRAME.registerComponent('initial-force', {
-    schema: {type: "vec3"},
-    init: function() {
+    schema: {type: 'vec3'},
+    init: function () {
         const {x, y, z} = this.data;
         this.el.addEventListener('body-loaded', (e) => {
-            debugger;
-            e.detail.body.applyForce(new CANNON.Vec3(x, y, z),new CANNON.Vec3(0, 0, 0));
+            e.detail.body.applyForce(new CANNON.Vec3(x, y, z), new CANNON.Vec3(0, 0, 0));
         });
 
 
     },
+});
+
+AFRAME.registerComponent('pushed-by-looking', {
+    schema: {force: {type: 'number', default: 1000}},
+    init: function () {
+        const force = this.data.force;
+        this.el.addEventListener('mouseenter', (e) => {
+            const camera = document.querySelector('a-camera');
+            const cameraPosition = camera.object3D.position.clone();
+            const intersectionPoint = e.detail.intersection.point.clone();
+
+            const pushVector = intersectionPoint.add(cameraPosition.negate());
+
+            const toApply = pushVector.multiplyScalar(force).add(new CANNON.Vec3(0, force, 0));
+            e.currentTarget.body.applyForce(toApply, new CANNON.Vec3(0, 0, 0));
+        })
+    }
 })
